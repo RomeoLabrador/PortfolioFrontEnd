5f9b1ff5fb858a4602808b94631733f8a1792405
RomeoLabrador <romeolabrador@hotmail.com.ar>
2022-11-29 15:30:00 -0300

    Header Terminado

--- Portfolio-Romeo-Labrador/src/app/app.module.ts
+++ Portfolio-Romeo-Labrador/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/header/logo/logo.component';
+import { SocialComponent } from './components/header/social/social.component';
+import { BannerComponent } from './components/header/banner/banner.component';
+import { LoginButtonComponent } from './components/header/login-button/login-button.component';
+import { PresentacionComponent } from './components/header/presentacion/presentacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    SocialComponent,
+    BannerComponent,
+    LoginButtonComponent,
+    PresentacionComponent
   ],
   imports: [
     BrowserModule,
